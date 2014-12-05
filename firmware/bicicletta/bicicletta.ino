76d78de462e10f76926445c24a816c4742d81d1d
Don Goodman-Wilson <dgoodman@railstars.com>
2014-12-05 12:08:33 -0800

    Compiles. Needs a small change to the BLE library for some reason.
    Add
    include "lib_aci.h" to aci_setup.h

--- firmware/bicicletta/bicicletta.ino
+++ firmware/bicicletta/bicicletta.ino
@@ -1,5 +1,24 @@
-#include <SPI.h>
+#include <stdint.h>
+#include <aci.h>
+#include <aci_cmds.h>
+#include <aci_evts.h>
+#include <aci_protocol_defines.h>
+#include <aci_queue.h>
+#include <acilib.h>
+#include <acilib_defs.h>
+#include <acilib_if.h>
+#include <acilib_types.h>
+#include <ble_assert.h>
+#include <boards.h>
+#include <dtm.h>
+#include <hal_aci_tl.h>
+#include <hal_platform.h>
+#include <lib_aci.h>
+#include <aci_setup.h>
+
 #include <RBL_nRF8001.h>
+
+#include <SPI.h>
 
 #include "bicicletta.h"
 #include "rs_ble.h"
--- firmware/bicicletta/rs_ble.h
+++ firmware/bicicletta/rs_ble.h
@@ -2,5 +2,6 @@
 #define __RS_BLE_H__
 
 void rs_ble_init(void);
+void rs_ble_process(void);
 
 #endif //__RS_BLE_H__
